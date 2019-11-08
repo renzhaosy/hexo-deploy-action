647a729a3e5aa2cd9e0c118ad20210c6bafebebe
renzhao <renzhao@xigua.city>
2019-11-08 13:10:47 +0800

    add commit and deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,45 +2,79 @@
 
 set -e
 
-echo "echo GITHUB_EVENT_PATH" && \
-echo $GITHUB_EVENT_PATH && \
+# check values
+if [ -n "${PUBLISH_REPOSITORY}" ]; then
+    PRO_REPOSITORY=${PUBLISH_REPOSITORY}
+else
+    PRO_REPOSITORY=${GITHUB_REPOSITORY}
+fi
 
-echo "echo GITHUB_ACTOR" && \
+if [ -z "$PUBLISH_DIR" ]
+then
+  echo "You must provide the action with the folder path in the repository where your compiled page generate at, example public."
+  exit 1
+fi
+
+if [ -z "$BRANCH" ]
+then
+  echo "You must provide the action with a branch name it should deploy to, for example master."
+  exit 1
+fi
+
+if [ -z "$PERSONAL_TOKEN" ]
+then
+  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  exit 1
+fi
+
+REPOSITORY_PATH="https://x-access-token:${PERSONAL_TOKEN}@github.com/${PRO_REPOSITORY}.git"
+
+# deploy to 
+echo "Deploy to ${PRO_REPOSITORY}"
 
 # Installs Git and jq.
 apt-get update && \
 apt-get install -y git && \
-apt-get install -y jq && \
-
-
-# Gets the commit email/name if it exists in the push event payload.
-COMMIT_EMAIL=`jq '.pusher.email' ${GITHUB_EVENT_PATH}`
-COMMIT_NAME=`jq '.pusher.name' ${GITHUB_EVENT_PATH}`
-
-# If the commit email/name is not found in the event payload then it falls back to the actor.
-if [ -z "$COMMIT_EMAIL" ]
-then
-  COMMIT_EMAIL="${GITHUB_ACTOR:-hexo-deploy-action}@users.noreply.github.com"
-fi
-
-if [ -z "$COMMIT_NAME" ]
-then
-  COMMIT_NAME="${GITHUB_ACTOR:-Hexo Deploy Action}"
-fi
 
 # Directs the action to the the Github workspace.
-cd $GITHUB_WORKSPACE && \
+cd $GITHUB_WORKSPACE 
 
-echo "Install hexo ..." && \
-npm install -g hexo hexo-deployer-git && \
+echo "npm install ..." 
+npm install
 
-echo "Clean folder ..." && \
 
-hexo clean && \
+echo "Clean folder ..."
+./node_modules/hexo/bin/hexo clean
 
-echo "Build and Deploy ..." && \
+echo "Generate file ..."
+./node_modules/hexo/bin/hexo generate 
 
-hexo deploy && \
+cd $PUBLISH_DIR
+
+echo "Config git ..."
+
+# Configures Git.
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git remote add origin "${REPOSITORY_PATH}"
+
+# Checks to see if the remote exists prior to deploying.
+# If the branch doesn't exist it gets created here as an orphan.
+# if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$BRANCH" | wc -l)" -eq 0 ];
+# then
+#   echo "Creating remote branch ${BRANCH} as it doesn't exist..."
+#   git checkout --orphan $BRANCH
+# fi
+
+git checkout --orphan $BRANCH
+
+git add --all
+
+echo 'Start Commit'
+git commit --allow-empty -m "Deploying to ${BRANCH}"
+
+echo 'Start Push'
+git push origin "${BRANCH}" --force
 
 echo "Deployment succesful!"
-
